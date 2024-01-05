97d4aaa1ca409df274553f4bc2baa26c55d36932
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-01-05 15:48:48 +0000

    fixed due to issue with contact form

--- src/app/(user)/contact/page.tsx
+++ src/app/(user)/contact/page.tsx
@@ -1,10 +1,9 @@
-import Contact from "@/components/Contact";
 
 
 export default function ContactPage() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-center p-24 bg-white'>
-      <Contact />
+      <p>hello</p>
     </main>
   );
 }
