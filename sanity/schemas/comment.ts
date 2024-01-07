6bbf3e5a26e7da7c9dcfde013784afacaa16b9dd
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-01-07 22:26:00 +0000

    sanity schema for comments added

---
+++ sanity/schemas/comment.ts
@@ -0,0 +1,30 @@
+import { defineType, defineField } from 'sanity';
+
+export default defineType({
+  name: 'comment',
+  title: 'Comment',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'email',
+      title: 'Email',
+      type: 'string',
+    }),
+    defineField({
+      name: 'comment',
+      title: 'Comment',
+      type: 'text',
+    }),
+    defineField({
+      name: 'post',
+      title: 'Post',
+      type: 'reference',
+      to: [{ type: 'post' }],
+    }),
+  ],
+});
