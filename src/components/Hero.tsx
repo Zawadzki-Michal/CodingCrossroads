b01b187a858dc5b90decbac607511330a5877040
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-01-08 14:41:08 +0000

    fix to button placement

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,14 +13,14 @@ const Hero = () => {
         alt="lorry"
         className="lg:w-full lg:max-h-[calc(100vh-80px)] lg:object-fill h-[calc(100vh-80px)] object-cover"
       />
-      <div className="absolute top-0 w-full h-full bg-black/40 text-gray-100 flex flex-col items-center justify-center lg:px-44 lg:gap-20 gap-20 text-center px-4">
+      <div className="absolute top-0 w-full h-full bg-black/40 text-gray-100 flex flex-col items-center justify-evenly lg:px-44 text-center px-4">
         <h2 className="text-6xl lg:text-[100px] font-bold text-center">
           Welcome to Coding Crossroads.
         </h2>
-        <p className="text-xl md:text-2xl lg:text-5xl font-semibold">
+        <p className="text-xl md:text-2xl lg:text-5xl font-semibold w-3/4">
           Join me on the ultimate journey from trucker to web developer.
         </p>
-        <p className="text-md md:text-2xl lg:text-xl font-semibold">
+        <p className="text-md md:text-2xl lg:text-xl font-semibold pb-12 w-3/4">
           Explore my coding bootcamp adventures, self-taught insights, and the
           twists and turns of my tech evolution. Fasten your seatbelt – the road
           to web mastery starts here!
