de88a2403ca333077c144ca3502dc25432b42844
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-01-08 14:54:28 +0000

    button removed for mobiles

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,10 +17,10 @@ const Hero = () => {
         <h2 className="text-6xl lg:text-[100px] font-bold text-center">
           Welcome to Coding Crossroads.
         </h2>
-        <p className="text-xl md:text-2xl lg:text-5xl font-semibold w-3/4">
+        <p className="text-xl md:text-2xl lg:text-5xl font-semibold md:w-3/4">
           Join me on the ultimate journey from trucker to web developer.
         </p>
-        <p className="text-md md:text-2xl lg:text-xl font-semibold pb-12 w-3/4">
+        <p className="text-md md:text-2xl lg:text-xl font-semibold pb-12 md:w-3/4">
           Explore my coding bootcamp adventures, self-taught insights, and the
           twists and turns of my tech evolution. Fasten your seatbelt – the road
           to web mastery starts here!
--- src/components/ScrollButton.tsx
+++ src/components/ScrollButton.tsx
@@ -15,7 +15,7 @@ const ScrollButton = () => {
   return (
     <button
       onClick={scrollToContent}
-      className="absolute lg:bottom-20 bottom-8 left-1/2 transform -translate-x-1/2 bg-transparent hover:text-gray-900 hover:bg-gray-50 text-gray-50 border-2 font-bold py-2 px-4 rounded"
+      className="hidden  md:block absolute lg:bottom-20 bottom-8 left-1/2 transform -translate-x-1/2 bg-transparent hover:text-gray-900 hover:bg-gray-50 text-gray-50 border-2 font-bold py-2 px-4 rounded"
     >
       Explore My Blog
     </button>
