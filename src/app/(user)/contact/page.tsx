f649f4265a66a259d5bcba659e85b1ecdd0f5171
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-01-09 00:24:57 +0000

    contact form

--- src/app/(user)/contact/page.tsx
+++ src/app/(user)/contact/page.tsx
@@ -13,8 +13,8 @@ export default function ContactPage() {
         alt="lorry"
         className="lg:w-full lg:max-h-[calc(100vh-80px)] lg:object-fill h-[calc(100vh-80px)] object-cover"
       />
-      <div className="absolute top-0 w-full h-full bg-black/40 text-gray-100 flex flex-col items-center justify-evenly lg:px-44 text-center px-4">
-        <div className="lg:w-1/2 lg:h-3/4 w-screen h-[calc(100vh-120px)] p-6  lg:bg-[#111111]/90   text-left lg:rounded-lg  flex flex-col gap-4 justify-between lg:p-10">
+      <div className="absolute top-0 w-full h-full bg-black/40 text-gray-100 flex flex-col items-center justify-between lg:justify-evenly lg:px-44 text-center px-4">
+        <div className="relative lg:w-1/2 lg:h-3/4 w-screen h-[calc(100vh-120px)] p-6  lg:bg-[#111111]/90   text-left lg:rounded-lg  flex flex-col gap-2 lg:gap-4 justify-between lg:p-10">
           <h2 className="text-6xl lg:text-6xl font-bold">How can I help?</h2>
 
           <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold">
