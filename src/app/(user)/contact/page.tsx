505fe35b3e63adc57d14a43009c4392336c5ea12
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-01-09 00:07:01 +0000

    responsivness for mobile

--- src/app/(user)/contact/page.tsx
+++ src/app/(user)/contact/page.tsx
@@ -14,13 +14,13 @@ export default function ContactPage() {
         className="lg:w-full lg:max-h-[calc(100vh-80px)] lg:object-fill h-[calc(100vh-80px)] object-cover"
       />
       <div className="absolute top-0 w-full h-full bg-black/40 text-gray-100 flex flex-col items-center justify-evenly lg:px-44 text-center px-4">
-        <div className="w-1/2 h-3/4  bg-[#111111]/90   text-left rounded-lg  flex flex-col gap-4 justify-between p-10">
+        <div className="lg:w-1/2 lg:h-3/4 w-screen h-screen p-6  bg-[#111111]/90   text-left lg:rounded-lg  flex flex-col gap-4 justify-between lg:p-10">
           <h2 className="text-6xl lg:text-6xl font-bold">How can I help?</h2>
 
           <h3 className="text-xl md:text-2xl lg:text-3xl font-semibold">
             {`I'd love to hear from you!`}
           </h3>
-          <p className="w-10/12">
+          <p className="w-10/12 text-md">
             {`Do you have a question or are you interested in working with me?
             Please fill out the form below and I'll get back to you as soon as I can.`}
           </p>
