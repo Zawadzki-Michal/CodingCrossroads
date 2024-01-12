63b0d7b81f7a8cf4da3859a8c55d0331635c631a
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-01-12 22:42:51 +0000

    Share on social media finished and styled

--- src/app/(user)/post/[slug]/page.tsx
+++ src/app/(user)/post/[slug]/page.tsx
@@ -1,4 +1,3 @@
-
 import { groq } from "next-sanity";
 import { client } from "../../../../../sanity/lib/client";
 import { Post } from "../../../../../types";
@@ -6,12 +5,12 @@ import Container from "@/components/Container";
 import Image from "next/image";
 import { urlFor } from "../../../../../sanity/lib/client";
 import Link from "next/link";
-import { FaGithub, FaLinkedin, FaWordpress } from 'react-icons/fa'
+import { FaGithub, FaLinkedin, FaWordpress } from "react-icons/fa";
 import { PortableText } from "@portabletext/react";
 import { RichText } from "@/components/RichText";
 import CommentBox from "@/components/CommentSection";
 import { Comment } from "../../../../../types";
-
+import SocialShare from "@/components/SocialShare";
 
 interface Props {
   params: {
@@ -31,14 +30,15 @@ export const generateStaticParams = async () => {
   }));
 };
 
-
 const CommentDisplay = ({ comment }: { comment: Comment }) => (
   <div className="comment">
-    <p><strong>{comment.name}</strong> ({new Date(comment._createdAt).toLocaleDateString()})</p>
+    <p>
+      <strong>{comment.name}</strong> (
+      {new Date(comment._createdAt).toLocaleDateString()})
+    </p>
     <p>{comment.comment}</p>
   </div>
 );
-
 
 const SlugPage = async ({ params: { slug } }: Props) => {
   const query = groq`*[_type == "post" && slug.current == $slug][0]{
@@ -55,48 +55,75 @@ const SlugPage = async ({ params: { slug } }: Props) => {
     }
   }`;
 
+  const baseUrl = "https://www.coding-crossroads.co.uk/post/";
+  const postUrl = baseUrl + slug;
+
   const post: Post = await client.fetch(query, { slug });
   console.log(post);
 
   return (
     <Container className="mb-10 lg:w-8/12 lg:mx-auto lg:m-10">
-      
       <div className="flex items-center justify-evenly mb-10">
         <div className="h-full md:w-1/2">
-          <Image src={urlFor(post?.mainImage).url()} width={400} height={400} alt="main image" className="object-cover w-full max-h-[460px]" />
+          <Image
+            src={urlFor(post?.mainImage).url()}
+            width={400}
+            height={400}
+            alt="main image"
+            className="object-cover w-full max-h-[460px]"
+          />
         </div>
         <div className="w-5/12 hidden md:inline-flex flex-col items-center gap-6 px-4">
-          <Image src={urlFor(post?.author?.image).url()} width={200} height={200} alt="author" className="rounded-full object-cover w-32 h-32" />
-          <p className="text-3xl text-purple-600 font-semibold">{post?.author?.name}</p>
-          <p className="text-xl text-gray-700 text-center tracking-wide">{post?.author?.description}</p>
-          
+          <Image
+            src={urlFor(post?.author?.image).url()}
+            width={200}
+            height={200}
+            alt="author"
+            className="rounded-full object-cover w-32 h-32"
+          />
+          <p className="text-3xl text-purple-600 font-semibold">
+            {post?.author?.name}
+          </p>
+          <p className="text-xl text-gray-700 text-center tracking-wide">
+            {post?.author?.description}
+          </p>
+
           <div className="flex items-center gap-4">
-          <Link href="https://github.com/Zawadzki-Michal" target='blank'>
-            <FaGithub className='text-2xl md:text-4xl text-gray-700 hover:text-gray-500 duration-200' />
-          </Link>
-          <Link href="https://www.linkedin.com/in/michal-zawadzki-12329394" target='blank'>
-            <FaLinkedin className='text-2xl md:text-4xl text-gray-700 hover:text-blue-400 duration-200' />
-          </Link>
-          <Link href="https://portfolio-sage-two-22.vercel.app/" target='blank'>
-            <FaWordpress className='text-2xl md:text-4xl text-gray-700 hover:text-blue-400 duration-200' />
-          </Link>
+            <Link href="https://github.com/Zawadzki-Michal" target="blank">
+              <FaGithub className="text-2xl md:text-4xl text-gray-700 hover:text-gray-500 duration-200" />
+            </Link>
+            <Link
+              href="https://www.linkedin.com/in/michal-zawadzki-12329394"
+              target="blank"
+            >
+              <FaLinkedin className="text-2xl md:text-4xl text-gray-700 hover:text-blue-400 duration-200" />
+            </Link>
+            <Link
+              href="https://portfolio-sage-two-22.vercel.app/"
+              target="blank"
+            >
+              <FaWordpress className="text-2xl md:text-4xl text-gray-700 hover:text-blue-400 duration-200" />
+            </Link>
           </div>
         </div>
-        
       </div>
-      <h1 className="lg:text-5xl text-3xl font-bold text-center lg:m-10 m-4 text-gray-700">{post?.title}</h1>
+      <h1 className="lg:text-5xl text-3xl font-bold text-center lg:m-10 m-4 text-gray-700">
+        {post?.title}
+      </h1>
       <span className="flex mx-3 mb-6    border-b  border-gray-700"></span>
       <div className="text-gray-700  font-normal lg:leading-10 leading-7  text-lg  lg:text-2xl mx-4 flex flex-col gap-5">
         <PortableText value={post?.body} components={RichText} />
       </div>
-      <div className="min-h-24">
-      <h1 className="lg:text-4xl text-3xl font-bold my-5 mx-3 text-gray-700">Comments</h1>
-      <span className="flex mx-3 mb-6    border-b  border-gray-700"></span>
+      <div className="min-h-24 md:flex flex flex-col-reverse gap-6 md:gap-2 md:flex-row  md:justify-between align-bottom md:pr-4">
+        <h1 className="lg:text-4xl text-3xl font-bold my-5 mx-3 text-gray-700">
+          Comments
+        </h1>
+        <SocialShare shareUrl={postUrl} title={post?.title} />
       </div>
+      <span className="flex mx-3 mb-6 mt-3    border-b  border-gray-700"></span>
       <div className="text-gray-700 lg:leading-10 leading-7  text-lg  lg:text-xl flex flex-col gap-5">
-      <CommentBox postId={post?._id} />
+        <CommentBox postId={post?._id} />
       </div>
-      
     </Container>
   );
 };
---
+++ src/components/SocialShare.tsx
@@ -0,0 +1,61 @@
+// Use client-side rendering
+"use client";
+// Import React and necessary components from react-share
+import React from "react";
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  LinkedinIcon,
+  WhatsappIcon,
+} from "react-share";
+
+// Define the props for the SocialShare component
+interface SocialShareProps {
+  shareUrl: string;
+  title: string;
+}
+
+// SocialShare component definition
+const SocialShare: React.FC<SocialShareProps> = ({ shareUrl, title }) => {
+  // Array of social platforms, each with a name, share button component, icon, and any extra props
+  const socialPlatforms = [
+    { name: "Facebook", Component: FacebookShareButton, Icon: FacebookIcon },
+    {
+      name: "Twitter",
+      Component: TwitterShareButton,
+      Icon: TwitterIcon,
+      extraProps: { title }, // Extra props for Twitter (e.g., title for the shared content)
+    },
+    { name: "LinkedIn", Component: LinkedinShareButton, Icon: LinkedinIcon },
+    { name: "Whatsapp", Component: WhatsappShareButton, Icon: WhatsappIcon },
+  ];
+
+  // Render the component
+  return (
+    <div className="display flex flex-col justify-center align-bottom items-center gap-2 text-gray-700 font-bold text-xl">
+      <p>{`Don't forget to share this post!`}</p>
+      <div className="social-share-buttons flex gap-5">
+        {/* Map through each social platform to render its share button and icon */}
+        {socialPlatforms.map(({ name, Component, Icon, extraProps }) => (
+          <div
+            key={name}
+            className="flex flex-col justify-items-center text-sm font-semibold text-gray-600"
+          >
+            {/* Render the share button component with its respective icon */}
+            <Component url={shareUrl} {...extraProps}>
+              <Icon size={40} round className="mx-auto py-1" />
+            </Component>
+            {name} {/* Display the name of the social platform */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// Export the SocialShare component
+export default SocialShare;
